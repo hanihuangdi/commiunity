fe1e460705a0dbae96bb025dfe43bd84cbc8cca4
xiaofu <1335224020@qq.com>
2019-09-26 09:47:20 +0800

    git properties modify

--- src/main/java/com/example/demo/contorller/AuthorizeController.java
+++ src/main/java/com/example/demo/contorller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.example.demo.dto.AccessTokenDTO;
 import com.example.demo.dto.GithubUser;
 import com.example.demo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +16,19 @@ import java.io.IOException;
 public class AuthorizeController {
     @Autowired
     GithubProvider gp;
+    @Value("${git.client.id}")
+    String Client_id;
+    @Value("${git.client.secret}")
+    String Client_secret;
+    @Value("${git.client.uri}")
+    String Redirect_uri;
     @RequestMapping("/callback")
     public String Authorize(@RequestParam(name="code") String code,@RequestParam(name="state") String state) throws IOException {
         AccessTokenDTO accessTokenDTO =new AccessTokenDTO();
-        accessTokenDTO.setClient_id("7e518a40f1fdb0465463");
-        accessTokenDTO.setClient_secret("9758643b90fb8ab2e60cf9aa25e65f0aa9933496");
+        accessTokenDTO.setClient_id(Client_id);
+        accessTokenDTO.setClient_secret(Client_secret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8085/callback");
+        accessTokenDTO.setRedirect_uri(Redirect_uri);
         accessTokenDTO.setState(state);
         String access_token = gp.getAccessToken(accessTokenDTO);
         GithubUser user = gp.getUser(access_token);
